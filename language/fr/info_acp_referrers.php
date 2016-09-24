eeb7953180da27b64b95dcba3cfa0e93626229aa
Raphaël M <Galixte@users.noreply.github.com>
2016-09-24 22:01:09 +0200

    Update of FRENCH translation for Referrers extension
    Hi @ForumHulp,
    
    could you merge this, please ?
    
    Changelog:
    - translate the new language keys added in last commits.

--- language/fr/info_acp_referrers.php
+++ language/fr/info_acp_referrers.php
@@ -1,12 +1,17 @@
 <?php
 /**
 *
-* @package Referrers
-* @copyright (c) 2014 ForumHulp.com
+* Referrers extension for the phpBB Forum Software package.
+* French translation by Galixte (http://www.galixte.com)
+*
+* @copyright (c) 2015 phpBB ForumHulp <http://www.forumhulp.com>
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
+/**
+* DO NOT CHANGE
+*/
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -17,18 +22,33 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
-// Description of extension
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
 	'DESCRIPTION_PAGE'		=> 'Description',
-	'DESCRIPTION_NOTICE'	=> 'Extension note',
+	'DESCRIPTION_NOTICE'	=> 'Note de l’extension',
 	'ext_details' => array(
 		'details' => array(
-			'DESCRIPTION_1'		=> 'Sortable',
-			'DESCRIPTION_2'		=> 'Whois function',
-			'DESCRIPTION_3'		=> 'Cron cleans table',
+			'DESCRIPTION_1'		=> 'Triable',
+			'DESCRIPTION_2'		=> 'Fonctionnalité Whois',
+			'DESCRIPTION_3'		=> 'Cron nettoie la table',
 		),
 		'note' => array(
-			'NOTICE_1'			=> 'phpBB 3.2 ready'
+			'NOTICE_1'			=> 'Prêt pour phpBB 3.2'
 		)
 	)
 ));
